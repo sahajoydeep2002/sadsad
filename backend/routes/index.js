d0bab81aa2e54f07d0f1406eef964bf1f3781497
JOY DEEP SAHA <88198830+sahajoydeep2002@users.noreply.github.com>
2022-06-12 16:55:24 +0530

    Delete backend directory

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function (req, res) {
-    res.send('Connected to the API');
-});
-
-module.exports = router;
